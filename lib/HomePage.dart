36ad675541a87619e25baf8d4cf0f9c40e278a3d
Charlotte-Miller <42000591+Charlotte-Miller@users.noreply.github.com>
2019-12-19 23:15:30 +0700

    Replace old calculation handler process with library that gives result base on expression

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,292 +1,177 @@
 import 'package:flutter/cupertino.dart';
+
+import './ExpressionEvaluation/math_expressions.dart';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import 'ColorScheme.dart';
 
 String output = "";
-String expressions = "";
+String expression = "";
 
-bool equalButtonPressed = false;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
 
-var numbers = new RegExp(r'[0-9]');
-var operators = new RegExp(r'/*-+^%');
-
-String calculatedValue = "";
-num firstNumber = 0;
-num secondNumber = 0;
-String operand = "";
-
-class MyHomePage extends StatefulWidget
-{
-    MyHomePage({Key key, this.title}) : super(key: key);
-    final String title;
-
-    @override
-    _MyHomePageState createState()
-    => _MyHomePageState();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage>
-{
-    @override
-    Widget build(BuildContext context)
-    {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
-            ),
-            body: new Container(
-
-                child: new Column(children: <Widget>[
-
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
 //Phạm Trung Hiếu
 //                    Display answer field
-                    new Container(
-
-                            alignment: Alignment.centerRight,
+            new Container(
+                alignment: Alignment.centerRight,
 
 //                        Modify answer's field margin
-                            padding: new EdgeInsets.symmetric(
-                                    vertical: 24.0,
-                                    horizontal: 12.0
-                            ),
+                padding:
+                    new EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
 
 //                            Modify answer's field style
-                            child: new Text(output,
-                                style: new TextStyle(
-                                        fontSize: 80.0,
-                                        fontWeight: FontWeight.bold
-                                ),
-                            )
-                    ),
+                child: new Text(
+                  output,
+                  style: new TextStyle(
+                      fontSize: 80.0, fontWeight: FontWeight.bold),
+                )),
 
 //                    Display expressions
-                    new Container(
-
-                            alignment: Alignment.centerRight,
+            new Container(
+                alignment: Alignment.centerRight,
 
 //                        Modify calculations' field margin
-                            padding: new EdgeInsets.symmetric(
-                                    horizontal: 12.0
-                            ),
+                padding: new EdgeInsets.symmetric(horizontal: 12.0),
 
 //                            Modify calculations' field style
-                            child: new Text(expressions,
-                                style: new TextStyle(
-                                    fontSize: 40.0,
-                                ),
-                            )
-                    ),
-
+                child: new Text(
+                  expression,
+                  style: new TextStyle(
+                    fontSize: 40.0,
+                  ),
+                )),
 
 //Trần Lê Hoàng
 //                    Put number rows to the bottom
-                    new Expanded(
-                        child: new Divider(),
-                    ),
+            new Expanded(
+              child: new Divider(),
+            ),
 
 //                    Build button rows
-                    buildFirstRow(),
-                    buildRowOf4('7', '8', '9', '*'),
-                    buildRowOf4('4', '5', '6', '-'),
-                    buildRowOf4('1', '2', '3', '+'),
-                    buildRowOf4('000', '0', '.', '='),
-
-                ],),
-            ),
-        );
-    }
+            buildFirstRow(),
+            buildRowOf4('7', '8', '9', '*'),
+            buildRowOf4('4', '5', '6', '-'),
+            buildRowOf4('1', '2', '3', '+'),
+            buildRowOf4('000', '0', '.', '='),
+          ],
+        ),
+      ),
+    );
+  }
 
 //    Special row for AC button and some operator buttons
-    Row buildFirstRow()
-    {
-        return new Row(
-                children: [
-                    buildButton('AC', backgroundColor: RUBY_RED),
-                    buildButton('^', backgroundColor: DARK_IMPERIAL_BLUE),
-                    buildButton('%', backgroundColor: DARK_IMPERIAL_BLUE),
-                    buildButton('/', backgroundColor: DARK_IMPERIAL_BLUE)
-                ]
-        );
-    }
+  Row buildFirstRow() {
+    return new Row(children: [
+      buildButton('AC', backgroundColor: RUBY_RED),
+      buildButton('(', backgroundColor: DARK_IMPERIAL_BLUE),
+      buildButton(')', backgroundColor: DARK_IMPERIAL_BLUE),
+      buildButton('/', backgroundColor: DARK_IMPERIAL_BLUE)
+    ]);
+  }
 
-    Row buildRowOf4(String first, second, third, fourth)
-    {
-        return new Row(
-                children: [
-                    buildButton(first),
-                    buildButton(second),
-                    buildButton(third),
-                    buildButton(fourth, backgroundColor: DARK_IMPERIAL_BLUE)
-                ]
-        );
-    }
+  Row buildRowOf4(String first, second, third, fourth) {
+    return new Row(children: [
+      buildButton(first),
+      buildButton(second),
+      buildButton(third),
+      buildButton(fourth, backgroundColor: DARK_IMPERIAL_BLUE)
+    ]);
+  }
 
-    Widget buildButton(String buttonNumber,
-                       {MaterialColor backgroundColor = MAASTRICHT_BLUE,
-                           MaterialColor textColor = BABY_POWDER})
-    {
-        return new Expanded(
-            child: new MaterialButton(
-                child: new Text(
-                    buttonNumber,
-
-                    style: new TextStyle(
-                        fontSize: 26.0,
-                        fontWeight: FontWeight.bold,
-                    ),
-                ),
-
-                onPressed: ()
-                => handleButtonPressed(buttonNumber),
-
-                color: backgroundColor,
-                textColor: textColor,
-                height: 100,
-            ),
-        );
-    }
+  Widget buildButton(String buttonNumber,
+      {MaterialColor backgroundColor = MAASTRICHT_BLUE,
+      MaterialColor textColor = BABY_POWDER}) {
+    return new Expanded(
+      child: new MaterialButton(
+        child: new Text(
+          buttonNumber,
+          style: new TextStyle(
+            fontSize: 26.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        onPressed: () => handleButtonPressed(buttonNumber),
+        color: backgroundColor,
+        textColor: textColor,
+        height: 100,
+      ),
+    );
+  }
 
 //    Nguyễn Việt Hoàng
-    handleButtonPressed(String buttonText)
+  handleButtonPressed(String buttonText) {
+    if (buttonText == 'AC') 
     {
-
-        if (equalButtonPressed && !buttonText.contains(numbers))
-        {
-            equalButtonPressed = false;
-
-            expressions = calculatedValue;
-        }
-
-        if (buttonText == "AC")
-        {
-            reset();
-        }
-        else if (buttonText == "+"
-                || buttonText == "-")
-        {
-            operand = buttonText;
-            firstNumber = num.tryParse(calculatedValue);
-            calculatedValue = "";
-            expressions += " ${operand} ";
-        }
-        else if (buttonText == "*"
-                || buttonText == "/")
-        {
-            operand = buttonText;
-            firstNumber = num.tryParse(output);
-            calculatedValue = "";
-
-            if (expressions.contains("+") || expressions.contains("-"))
-            {
-                expressions = "(${expressions}) ${operand} ";
-            }
-            else
-            {
-                expressions += " ${operand} ";
-            }
-        }
-        else if (buttonText == ".")
-        {
-            if (calculatedValue.contains("."))
-            {
-                print("You already pressed the dot button");
-                return;
-            }
-            else
-            {
-                calculatedValue += buttonText;
-                expressions += buttonText;
-            }
-        }
-        else if (buttonText == "=" && !equalButtonPressed)
-        {
-            equalButtonPressed = true; //to block spamming press "=" button that leads to misleading output display
-
-            secondNumber = num.tryParse(calculatedValue);
-
-            calculatedValue = getAnswerByOperand(firstNumber, secondNumber, operand);
-        }
-        else if (equalButtonPressed && buttonText.contains(numbers))
-        {
-            reset();
-
-            calculatedValue += buttonText;
-            expressions += buttonText;
-        }
-        else if (buttonText.contains(numbers)) //when press number buttons
-        {
-            calculatedValue += buttonText;
-            expressions += buttonText;
-        }
-
-        print(calculatedValue);
-
-        setDisplayState(calculatedValue);
+      reset();
+    } 
+    else if (buttonText != '=') 
+    {
+      expression += " $buttonText ";
+    } 
+    else if (buttonText == '=') 
+    {
+      double result = getResultBaseOnExpression(expression);
+      output = getFormattedContent(result);
     }
 
-    String getAnswerByOperand(num firstNumber, num secondNumber, String operand)
-    {
-        switch (operand)
-        {
-            case "+":
-                return (firstNumber + secondNumber).toString();
+    setDisplayState(output);
+  }
 
-            case "-":
-                return (firstNumber - secondNumber).toString();
+  void reset() {
+    expression = "";
+    output = "";
+  }
 
-            case "*":
-                return (firstNumber * secondNumber).toString();
+  double getResultBaseOnExpression(String expression) {
+    Parser p = new Parser();
+    Expression exp = p.parse(expression);
 
-            case "/":
-                return (firstNumber / secondNumber).toString();
+    ContextModel cm = new ContextModel();
 
-            default:
-                return "Undifined operand";
-        }
+    return exp.evaluate(EvaluationType.REAL, cm);
+  }
+
+  String getFormattedContent(content) {
+    content = content.toString();
+
+    if (content == "") {
+      return content;
+    } else {
+      num finalOutput = num.tryParse(content);
+      var formatter = new NumberFormat();
+
+      if (finalOutput is int) //Check if output is an integer
+      {
+        return formatter.format(finalOutput);
+      } else {
+        num roundedOutput = num.tryParse(finalOutput.toStringAsFixed(
+            3)); //round the output to 2 digits after decimal point
+        return formatter.format(roundedOutput);
+      }
     }
+  }
 
-    void setDisplayState(String content)
-    {
-        setState(()
-        {
-            output = getFormattedContent(content);
-        });
-    }
-
-    String getFormattedContent(content)
-    {
-        if (content == "")
-        {
-            return content;
-        }
-        else
-        {
-            num finalOutput = num.tryParse(content);
-            var formatter = new NumberFormat();
-
-            if (finalOutput is int) //Check if output is an integer
-            {
-                return formatter.format(finalOutput);
-            }
-            else
-            {
-                num roundedOutput = num.tryParse(finalOutput
-                        .toStringAsFixed(2)); //round the output to 2 digits after decimal point
-                return formatter.format(roundedOutput);
-            }
-        }
-    }
-
-    void reset()
-    {
-        expressions = "";
-        output = "";
-        calculatedValue = "";
-        firstNumber = 0.0;
-        secondNumber = 0.0;
-        operand = "";
-        equalButtonPressed = false;
-    }
+  void setDisplayState(String content) {
+    setState(() {
+      output = content;
+    });
+  }
 }
